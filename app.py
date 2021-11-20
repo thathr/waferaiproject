d4c99721a5018a9db4c9e6f31e38d2ad1e994b8e
ranga <rangaraja.thatha@dell.com>
2021-11-20 18:23:21 +0530

    changed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Flask app is running raja"
 
 
 if __name__ == "__main__":
